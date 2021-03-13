82d402a834547d63510920f465fbc5b5062071e1
Abdul Moeed Saqib <sahilsaqib3333@gmail.com>
2021-03-13 18:35:32 -0500

    Updated
    updated project

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -28,21 +28,16 @@ public class EnemyScript : MonoBehaviour
     bool isPlayerInRange;
     bool isPlayerInAttackRange;
 
-<<<<<<< HEAD
-=======
     [SerializeField]
     Sound[] attackSounds;
 
->>>>>>> e069ea7d583280e84d72f8567210611914636200
-
+    //f
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         player = GameObject.Find("Player").transform;
-<<<<<<< HEAD
-=======
 
         for (int i = 0; i < attackSounds.Length; i++)
         {
@@ -51,7 +46,7 @@ public class EnemyScript : MonoBehaviour
             attackSounds[i].SetSource(_go.AddComponent<AudioSource>());
 
         }
->>>>>>> e069ea7d583280e84d72f8567210611914636200
+
     }
 
     // Update is called once per frame
@@ -115,12 +110,10 @@ public class EnemyScript : MonoBehaviour
         Vector3 direction = (player.position - transform.position).normalized;
         var rotateToTarget = Quaternion.LookRotation(new Vector3(direction.x, 0F, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, rotateToTarget, rotateSpeed * Time.deltaTime);
-<<<<<<< HEAD
-=======
 
         var rand = UnityEngine.Random.Range(0, attackSounds.Length - 1);
         attackSounds[rand].Play();
->>>>>>> e069ea7d583280e84d72f8567210611914636200
+
     }
 
     private void SearchWalkPoint()
