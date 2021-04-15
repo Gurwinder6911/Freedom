255114f3d7d59757fef42322f924b7592577ff0a
Guizuka <51218840+Guizuka@users.noreply.github.com>
2021-04-15 09:06:50 -0300

    Created 3 cave levels

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -66,7 +66,7 @@ public class PlayerScript : MonoBehaviour
             CheckingOnGround();
             PlayerMovement();
             RunningAnimation();
-            //PlayerJump();
+            PlayerJump();
         }
     }
 
